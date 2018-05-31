ce89e221d7f57d6bf7c0f51dde9a1e0281a49ae7
gholk <85619andy@gmail.com>
2018-05-31 19:36:52 +0800

    wait emacs start when restart emacs for scim

--- emacs-with-scim.sh
+++ emacs-with-scim.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+
+wait_emacs_start() {
+    while sleep 1s
+    do
+        if systemctl --user status emacs >/dev/null
+        then break
+        fi
+    done
+}
+
+wait_emacs_start
+
 systemctl --user restart emacs.service
 emacsclient -c --no-wait
